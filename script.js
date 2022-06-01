f7a747842d7b9f89a9b7716744a4b20f37d895f1
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-06-01 17:03:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*"strict use"
+  -Utilizando esse método eu evito problemas de :
+  -Variáveis que eram para ser em escopo local virem escopo global
+por esquecer de declarar a variável
+*/
+
+ "use strict";
+
+ x = 7;
+
+console.log(window);
